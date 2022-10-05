3efb5ddcd0d0cdcf62986fa7a8e1073d5d6453f6
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-10-05 10:15:59 +0900

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,9 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 # shellcheck disable=SC2164,SC2034
 cd "${HOME}"
 dir_check="${HOME}/.otl"
 file_check="${HOME}/.otl/otl"
+
+sel() {
+  PS3="입력 : "
+  # shellcheck disable=SC2068
+  select item in "최신 버전으로 재설치" $@; do
+    echo "$item"
+    break
+  done
+}
 
 download() {
     rm -rf .otl
@@ -34,16 +43,13 @@ download() {
 if [ -d "$dir_check" ]; then
   if [ -f "$file_check" ]; then
     echo "이미 설치되어 있습니다."
-      echo "1) 최신 버전으로 재설치"
-      echo "2) 취소"
-      # shellcheck disable=SC2162
-      read menu
-      # shellcheck disable=SC2039
-      if [ "$menu" == 1 ]; then
-        download
-      else
-        exit 0
-      fi
+    # shellcheck disable=SC2006
+    chose=`sel "취소"`
+    if [ "$chose" == "최신 버전으로 재설치" ]; then
+      download
+    else
+      exit 0
+    fi
   else
     echo "${HOME}/.otl 경로는 otl에서 사용하는 경로 입니다. 해당 디렉토리를 이름 변경이나 이동해주세요."
   fi
