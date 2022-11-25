933c6f60d5d35d9e73a6b33293e11db963444ca7
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-11-25 09:31:59 +0900

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# shellcheck disable=SC2164,SC2034
+# shellcheck disable=SC2164,SC2034,SC2168,SC2081,SC2016,SC2006
+OTL_HOME="${HOME}/.otl"
+
+# shellcheck disable=SC2089,SC2090
+file=`cat ~/.zshrc`
+otl_home='export OTL_HOME="${HOME}/.otl"'
+if [[ "${file}" != *"${otl_home}"* ]]; then
+  echo "$otl_home" >>  ~/.zshrc
+fi
+
 cd "${HOME}"
-dir_check="${HOME}/.otl"
-file_check="${HOME}/.otl/otl"
+dir_check="${OTL_HOME}"
+file_check="${OTL_HOME}/otl"
 download_version="$1"
 
 download() {
@@ -11,10 +20,10 @@ download() {
     rm -rf .otl
     if [ -z "$download_version" ]; then
       git clone https://github.com/OTLanguage/.otl.git
-      rm -r ${HOME}/.otl/.git
-      module_zip="${HOME}/.otl/run-tool/lib/modules.zip"
-      unzip "${module_zip}" -d "${HOME}/.otl/run-tool/lib"
-      if [ -e "${HOME}/.otl/run-tool/lib/modules" ]; then
+      rm -r "${OTL_HOME}"/.git
+      module_zip="${OTL_HOME}/run-tool/lib/modules.zip"
+      unzip "${module_zip}" -d "${OTL_HOME}/run-tool/lib"
+      if [ -e "${OTL_HOME}/run-tool/lib/modules" ]; then
         rm -f "${module_zip}"
       else
         echo "modules 생성에 실패하였습니다."
@@ -29,15 +38,14 @@ download() {
       fi
     fi
 
-    if [ -f "${HOME}/.otl/otl" ]; then
-      chmod +x "${HOME}/.otl/otl"
+    if [ -f "${OTL_HOME}/otl" ]; then
+      chmod +x "${OTL_HOME}/otl"
       # shellcheck disable=SC2006
       file=`cat ~/.zshrc`
-      zs_value=alias otl="sh ${HOME}/.otl/otl"
+      zs_value='alias otl="sh ${HOME}/.otl/otl"'
       # shellcheck disable=SC2039,SC2081
-      if [ "${file}" != *"${zs_value}"* ]; then
-        # shellcheck disable=SC2016
-        echo 'alias otl="sh ${HOME}/.otl/otl"' >>  ~/.zshrc
+      if [[ "${file}" != *"${zs_value}"* ]]; then
+        echo "$zs_value" >>  ~/.zshrc
       fi
       echo "설치가 완료되었습니다. 터미널을 재시작하시면 세팅이 적용됩니다."
     else
@@ -51,7 +59,7 @@ if [ -d "$dir_check" ]; then
     echo "재설치를 시작합니다."
     download
   else
-    echo "${HOME}/.otl 경로는 otl에서 사용하는 경로 입니다. 해당 디렉토리를 이름 변경이나 이동해주세요."
+    echo "${OTL_HOME} 경로는 otl에서 사용하는 경로 입니다. 해당 디렉토리를 이름 변경이나 이동해주세요."
   fi
 else
   download
